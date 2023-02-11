757378155d16c06c284e0cbcf29f989f2dbb8e86
Picasso <thecodepicasso@gmail.com>
2023-02-11 14:53:40 -0800

    modified colors andd app json

--- src/style/colors.js
+++ src/style/colors.js
@@ -2,8 +2,9 @@ export const COLORS = {
   white: "#fff",
   black: "#000",
   grey: "#D3D3D3",
-  lightGrey: "rgba(141, 141, 144, 0.4)",
-  red: "#990000",
+  red: "#44173f",
+  lightRed: "#802F56",
+  transRed: "#E1D0D0",
+  pink: "#A44A8A",
   green: "#93EB4B",
-  blue: "#1E4B6C",
 };
