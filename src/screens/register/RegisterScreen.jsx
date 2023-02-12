a337b39184efdb7a4287d44c123c1ae94916e8e1
Picasso <thecodepicasso@gmail.com>
2023-02-12 01:13:46 -0800

    completed jsx and styling for register screen

--- src/screens/register/RegisterScreen.jsx
+++ src/screens/register/RegisterScreen.jsx
@@ -1,11 +1,44 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
-
+import Wrapper from "../../components/wrapper/Wrapper";
+import styles from "./Style";
+import { screenHeight } from "../../style/metrics";
+import Card from "../../components/card/Card";
+import Input from "../../components/input/Input";
+import Button from "../../components/button/Button";
 const RegisterScreen = () => {
+  const { card, img, inputOne, inputTwo, inputThree, loginBtn, loginBtnTxt } =
+    styles;
   return (
-    <View>
-      <Text>RegisterScreen</Text>
-    </View>
+    <Wrapper>
+      <View
+        style={{
+          flex: 0.2,
+          marginTop: screenHeight / 12,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Image source={require("../../imgs/spalsh-img.png")} style={img} />
+      </View>
+      <View
+        style={{
+          flex: 0.5,
+          marginTop: 30,
+        }}
+      >
+        <Card cardStyle={card}>
+          <Input inputStyle={inputOne} placeholder="Name" />
+          <Input inputStyle={inputTwo} placeholder="Password" />
+          <Button
+            btnStyle={loginBtn}
+            title="Login"
+            txtStyle={loginBtnTxt}
+            onPress={() => console.log("Hello")}
+          />
+        </Card>
+      </View>
+    </Wrapper>
   );
 };
 
--- src/screens/register/Style.js
+++ src/screens/register/Style.js
@@ -1,4 +1,66 @@
 import { StyleSheet } from "react-native";
 import { COLORS, SIZES, screenHeight, screenWidth } from "../../style/index";
-const styles = StyleSheet.create({});
+import { isIos } from "../../style/metrics";
+const styles = StyleSheet.create({
+  card: {
+    width: screenWidth / 1.2,
+    height: screenHeight / 2,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    backgroundColor: COLORS.red,
+    borderRadius: 20,
+    flex: 1,
+  },
+  img: {
+    width: isIos ? screenWidth / 1.85 : screenWidth / 1.4,
+    height: isIos ? screenHeight / 3.75 : screenHeight / 3.1,
+  },
+  inputOne: {
+    width: "80%",
+    borderBottomColor: COLORS.white,
+    borderWidth: 1,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    padding: 10,
+    color: COLORS.white,
+    fontSize: SIZES.m,
+  },
+  inputTwo: {
+    width: "80%",
+    borderBottomColor: COLORS.white,
+    borderWidth: 1,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    padding: 10,
+    color: COLORS.white,
+    fontSize: SIZES.m,
+  },
+  inputThree: {
+    width: "80%",
+    borderBottomColor: COLORS.white,
+    borderWidth: 1,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    padding: 10,
+    color: COLORS.white,
+    fontSize: SIZES.m,
+  },
+  loginBtn: {
+    width: "80%",
+    height: "18%",
+    backgroundColor: COLORS.pink,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 50,
+  },
+  loginBtnTxt: {
+    color: COLORS.white,
+    fontSize: 20,
+    fontWeight: "600",
+    fontfamily: "Inter_700Bold",
+  },
+});
 export default styles;
